e5d43fee4f08d1c12a4e5d7553a5981108a538d2
tk84 <git@tk84.net>
2011-10-24 15:11:36 +0900

    便利関数の寄せ集め

---
+++ MyFunction.rb
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+require 'digest'
+module MyFunction
+  def self.uniqid
+    Digest::MD5.hexdigest('tk84' + Time.now.instance_eval { '%s.%06d' % [strftime('%Y%m%d%H%M%S'), usec] })
+  end
+end

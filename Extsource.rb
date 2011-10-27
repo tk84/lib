7f62833f42e0eaa5a6785b28c0cb3bc3823cebaf
tk84 <git@tk84.net>
2011-10-28 03:41:02 +0900

    外部リソースマネージメントクラスExtsourceの追加

---
+++ Extsource.rb
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+require 'singleton'
+module Tk84
+  class Extsource
+    include Singleton
+
+    def initialize
+      @source = {}
+      @parser = {}
+      @parsed = {}
+    end
+
+    def method_missing type, *args
+      id = args.shift
+
+      if not @parsed[type][id]
+        parser = @parser[type].new
+        parser.parse(@source[type][id])
+        @parsed[type][id] = parser
+      elsif
+        parser = @parsed[type][id]
+      end
+
+      parser.result(*args)
+    end
+
+    def parser type, parser
+      @parser.store type.to_sym, parser
+      @parsed[type] = {} if not @parsed[type]
+    end
+
+    def source type, *args
+      (args[0].kind_of?(Hash) ? args[0] : Hash[*args]).each_pair do |key, value|
+        @parsed[type][key] = nil if @parsed[type] && @parsed[type][key]
+        @source[type] = {} if not @source[type]
+        @source[type][key] = value
+      end
+    end
+
+    def debug
+      p @source
+      p @parser
+      p @parsed
+    end
+
+    class Sql
+      def initialize
+        @result = {}
+      end
+
+      def parse source
+        res = nil
+        File.open(source, 'r') do |file|
+          contents = file.read
+          contents.scan /(?=^|\n)--:([\w]+)((?!\n--:[\w+])\n[^\n]*)*/ do |s|
+            id = Regexp.last_match(1).to_sym
+            @result.store id, Regexp.last_match(0)
+          end
+        end
+      end
+
+      def result id, placeholder={}
+        result = @result[id]
+
+        placeholder.each_pair do |key,value|
+          result.gsub! "\#{#{key}}", value
+        end
+
+        result
+      end
+    end
+  end
+end
--- MyFunction.rb
+++ MyFunction.rb
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 require 'digest'
-module MyFunction
-  def self.uniqid
-    Digest::MD5.hexdigest('tk84' + Time.now.instance_eval { '%s.%06d' % [strftime('%Y%m%d%H%M%S'), usec] })
+module Tk84
+  module MyFunction
+    def self.uniqid
+      Digest::MD5.hexdigest('tk84' + Time.now.instance_eval { '%s.%06d' % [strftime('%Y%m%d%H%M%S'), usec] })
+    end
   end
 end
